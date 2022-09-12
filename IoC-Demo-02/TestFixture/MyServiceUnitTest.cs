254b0ee09e3d1ffaf4210ae5d0bc91b6475476bf
KBR <kbr@ucl.dk>
2022-09-12 15:26:03 +0200

    DoService_UnitTest

---
+++ IoC-Demo-02/TestFixture/MyServiceUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using Services;
+
+namespace TestFixture;
+
+public class MyServiceUnitTest
+{
+    [Fact]
+    public void DoService_UnitTest()
+    {
+        // Arrange
+        var myHelper = new Mock<IMyHelper>();
+        var sut = new MyService(myHelper.Object);
+
+        // Act
+        sut.DoService();
+
+        // Assert
+    }
+}
